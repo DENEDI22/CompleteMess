0b9cf61c1d7e7da4905dc28f93a87b2a837759e5
Christian <chrille_0313@hotmail.com>
2021-08-22 15:41:33 +0200

    Added Weapon System

---
+++ Assets/Scripts/Characters/CharController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+
+    [Header("Movement")]
+    public float speed = 10f;
+
+
+    public void Move(Vector2 direction, float amount)
+    {
+
+    }
+
+
+
+}

377bd64df517ab398cf32a1565be7be68acd990d
Daniel Liubymyi <denedi2004@gmail.com>
2021-08-22 18:52:01 +0300

    Cinemachine Camera added

--- Assets/Scripts/PlayerMoving.cs
+++ Assets/Scripts/PlayerMoving.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,13 +7,17 @@ namespace PlayerControls
     [RequireComponent(typeof(PlayerInput))]
     public class PlayerMoving : MonoBehaviour
     {
+        private void Awake()
+        {
+            FindObjectOfType<Cinemachine.CinemachineTargetGroup>().AddMember(transform, 1, 1);
+        }
 
         public void OnMove(InputAction.CallbackContext _context)
         {
             Debug.Log($"Player OnMove invoked with axis: x: {_context.ReadValue<Vector2>().x} and y {_context.ReadValue<Vector2>().y}");
-            transform.Translate(_context.ReadValue<Vector2>());
+            transform.Translate(_context.ReadValue<Vector2>().x, 0, _context.ReadValue<Vector2>().y);
         }
-
+        
         public void OnFire(InputAction.CallbackContext _context)
         {
             Debug.Log("FIRE!");

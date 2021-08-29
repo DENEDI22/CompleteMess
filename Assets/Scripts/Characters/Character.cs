1b4a3a6a1f1d3c126e61a079499aaab20c65fc85
Daniel Liubymyi <denedi2004@gmail.com>
2021-08-29 05:28:38 +0300

    Health bars added and damage anim fixed

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -1,22 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(HealthBar))]
 [RequireComponent(typeof(CharController))]
 public class Character : MonoBehaviour
 {
 
     public CharacterStats characterStats;
     private CharController characterController;
-
+    private HealthBar healthBar;
     public event Action deathEvent;
     public UnityEvent onTakeDamage;
     private void Awake()
     {
         FindObjectOfType<Cinemachine.CinemachineTargetGroup>().AddMember(transform, 1, 1);
-
+        healthBar = GetComponent<HealthBar>();
         characterStats = Instantiate(characterStats);
         characterController = gameObject.GetComponent<CharController>();
     }
@@ -24,19 +26,25 @@ public class Character : MonoBehaviour
     private void Start()
     {
         characterStats.characterController = characterController;
+        healthBar.SetMaxHealth(characterStats.health);
     }
 
     public void Die()
     {
         deathEvent?.Invoke();
-        Destroy(gameObject);
+        Destroy(characterController);
+        GetComponent<PlayerMovement>().enabled = false;
+        GetComponent<PlayerWeaponManagement>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        FindObjectOfType<CinemachineTargetGroup>().RemoveMember(transform);
+        transform.position = Vector3.down * 10;
     }
 
     public void TakeDamage(float damage)
     {
         characterStats.health -= damage;
         onTakeDamage.Invoke();
-
+        healthBar.SetHealth(characterStats.health);
         if (characterStats.health <= 0)
         {
             Die();
